91448f6b709e87267d392d67962fa71f84ea2598
jayesh00041 <jayesh00041@gmail.com>
2022-08-31 11:42:06 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a4a91b2a200f8c17dd363fda160e68ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fefb4538d9a17008dc5e",
+    "url": "/React-ecommerce/static/css/main.e6673596.chunk.css"
+  },
+  {
+    "revision": "fefb4538d9a17008dc5e",
+    "url": "/React-ecommerce/static/js/main.fefb4538.chunk.js"
+  },
+  {
+    "revision": "079a51e1eca575ceb4bf",
+    "url": "/React-ecommerce/static/js/1.079a51e1.chunk.js"
+  },
+  {
+    "revision": "e698f7258b448489c234",
+    "url": "/React-ecommerce/static/css/2.dee2b268.chunk.css"
+  },
+  {
+    "revision": "e698f7258b448489c234",
+    "url": "/React-ecommerce/static/js/2.e698f725.chunk.js"
+  },
+  {
+    "revision": "0cd372dcbfa04ae0067b",
+    "url": "/React-ecommerce/static/js/runtime~main.0cd372dc.js"
+  },
+  {
+    "revision": "62cd52ae7e55e9297247da3b5c3dfddb",
+    "url": "/React-ecommerce/index.html"
+  }
+];
